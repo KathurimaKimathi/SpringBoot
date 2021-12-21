ca14c03a47d1bf1f31037248c671fd5d7d2c5fe7
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2021-12-21 12:55:42 +0300

    feat:add CRUD REST APIs
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

---
+++ SpringBoot/springbootcrudrest/src/test/java/com/kathurima/springbootcrudrest/SpringbootcrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kathurima.springbootcrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
