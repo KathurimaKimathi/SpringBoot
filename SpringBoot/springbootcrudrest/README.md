37c7c0a159d0562f51870e0d140eeae7485e4a4d
Kathurima Kimathi <41376826+KathurimaKimathi@users.noreply.github.com>
2021-12-27 15:19:49 +0300

    chore:add unit tests (#1)
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

---
+++ SpringBoot/springbootcrudrest/README.md
@@ -0,0 +1,13 @@
+# WHAT WE ARE BUILDING
+
+We are building a simple CRUD User Management Application Rest APIs.
+
+## Tools and Technologies Used
+- Spring Boot - 2.0.4.RELEASE
+- JDK - 1.8 or later
+- Spring Framework - 5.0.8 RELEASE
+- Hibernate - 5.2.17.Final
+- Maven - 3.2+
+- Spring Data JPA - 2.0.10 RELEASE
+- IDE - Eclipse or Spring Tool Suite(STS)
+- MYSQL - 5.1.47
--- SpringBoot/springbootcrudrest/src/main/java/com/kathurima/springbootcrudrest/SpringbootcrudrestApplication.java
+++ SpringBoot/springbootcrudrest/src/main/java/com/kathurima/springbootcrudrest/SpringbootcrudrestApplication.java
@@ -2,8 +2,10 @@ package com.kathurima.springbootcrudrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootcrudrestApplication {
 
 	public static void main(String[] args) {
